2c90cbf911631808d43df30583dc4a3d2223698a
HeatherStAmand <32377329+HeatherStAmand@users.noreply.github.com>
2018-01-06 16:29:45 -0500

    Add files via upload

---
+++ starter.js
@@ -0,0 +1,5 @@
+let contactStarter = [
+    {"fullname":"Han Solo", "phone":"613-727-4723", "email":"han.solo@falcon.com"},
+    {"fullname":"Luke Skywalker", "phone":"613-321-8765", "email":"luke.skylwalker@jedi.org"},
+    {"fullnam":"Kylo Ren", "phone":"613-876-9900", "email":"kylo.ren@firstorder.io"}
+];
